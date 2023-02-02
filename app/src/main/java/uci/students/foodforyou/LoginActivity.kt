5a8a817fc9cd638574d03503fba84d3ea17d250b
Jason Tran <jasont787@yahoo.com>
2023-02-01 18:31:03 -0800

    Added login functionality
    Able to login with an account with Firebase which will bring you to the main activity. The app has no implementation of creating a new account yet. Accounts have to be made on the Firebase console/website itself.

--- app/src/main/java/uci/students/foodforyou/LoginActivity.kt
+++ app/src/main/java/uci/students/foodforyou/LoginActivity.kt
@@ -1,30 +1,67 @@
 package uci.students.foodforyou
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
+
 class LoginActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
-
+    private var currentUser: FirebaseUser? = null
     private lateinit var etUsername:EditText
     private lateinit var etPassword:EditText
+    private lateinit var btnLogin: Button
+    private val TAG="LoginActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         etUsername=findViewById<EditText>(R.id.etUsername)
         etPassword=findViewById<EditText>(R.id.etPassword)
+        btnLogin=findViewById<Button>(R.id.btnLogin)
+
+        btnLogin.setOnClickListener { loginUser() }
         auth= Firebase.auth
+
+        currentUser=auth.currentUser
+        if (currentUser!=null){
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
 
 
     }
     fun loginUser(){
-        var name=etUsername.text.toString()
-        var password=etPassword.text.toString()
+        val name=etUsername.text.toString()
+        val password=etPassword.text.toString()
+
+        auth.signInWithEmailAndPassword(name,password).addOnCompleteListener(this)
+        {task ->
+            if (task.isSuccessful) {
+                // Log the user in if successful
+                Log.d(TAG, "signInWithEmail:success")
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                //TODO Change screen to the main menu
+            } else {
+                // Display an error message if failed
+                Log.w(TAG, "signInWithEmail:failure", task.exception)
+                Toast.makeText(baseContext, "Failed to sign in",
+                    Toast.LENGTH_SHORT).show()
+
+            }
+
+        }
 
     }
 }
--- app/src/main/java/uci/students/foodforyou/MainActivity.kt
+++ app/src/main/java/uci/students/foodforyou/MainActivity.kt
@@ -1,11 +1,26 @@
 package uci.students.foodforyou
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    private val TAG="MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val logoutBtn=findViewById<Button>(R.id.btnLogOutTemp)
+        val auth= Firebase.auth
+        logoutBtn.setOnClickListener {
+            auth.signOut()
+            Log.d(TAG,"Logging user out and return to login menu")
+            intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
